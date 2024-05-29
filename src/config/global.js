96013f8197ad244f1a9aa0e56da6369e00aa6160
emilydev <emilse.a.b@gmail.com>
2024-05-28 23:29:55 -0500

    se vincula descargable

--- src/config/global.js
+++ src/config/global.js
@@ -97,7 +97,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228118_CF29_DU.pdf',
       },
       {
         icono: 'fas fa-download',
