2bd606060e021f2243f1d73ea0398c718ed6fc38
emilydev <emilse.a.b@gmail.com>
2024-05-29 00:57:05 -0500

    se actualizan créditos

--- src/config/global.js
+++ src/config/global.js
@@ -253,6 +253,11 @@ export default {
           cargo: 'Actividad Didáctica',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
+        {
+          nombre: 'Daniela Muñoz Bedoya',
+          cargo: 'Animador y Producción audiovisual',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
       ],
     },
     {
