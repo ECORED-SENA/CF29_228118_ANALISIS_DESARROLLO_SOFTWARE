3646ca0939a5ce81a1fb9093c6d2addb52be6802
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-07-03 12:30:24 -0500

    Update Fuentes

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5.bgs.p-5.brad
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="La identificación de requerimientos se realiza al inicio de un proyecto de #[em software], y tiene dos tipos: Funcionales: lo que se espera del usuario. No funcionales: compuesto por los requerimientos del proceso, organizacionales y externos. Las plataformas de desarrollo e implementación de aplicaciones y servicios, son: LAMP, WAMP, XAMPP, WXCF, XATMJ, WIMA, máquinas virtuales y contenedores.")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="La identificación de requerimientos se realiza al inicio de un proyecto de software, y tiene dos tipos: Funcionales: lo que se espera del usuario. No funcionales: compuesto por los requerimientos del proceso, organizacionales y externos. Las plataformas de desarrollo e implementación de aplicaciones y servicios, son: LAMP, WAMP, XAMPP, WXCF, XATMJ, WIMA, máquinas virtuales y contenedores.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
